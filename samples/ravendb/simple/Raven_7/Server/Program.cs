c5046889e7dc581e9546e1f31fae2c435f5da97d
Daniel Marbach <daniel.marbach@openplace.net>
2022-12-01 12:36:38 +0100

    Use same dependencies

--- samples/ravendb/simple/Raven_7/Server/Program.cs
+++ samples/ravendb/simple/Raven_7/Server/Program.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
 using NServiceBus;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Operations.Expiration;
@@ -17,7 +22,26 @@ class Program
 
         #region Config
 
+        var config = new LoggingConfiguration();
+
+        var consoleTarget = new ColoredConsoleTarget
+        {
+            Layout = "${level}|${logger}|${message}${onexception:${newline}${exception:format=tostring}}"
+        };
+        config.AddTarget("console", consoleTarget);
+        config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+
+        LogManager.Configuration = config;
+
+        NServiceBus.Logging.LogManager.Use<NLogFactory>();
+
+        var container = new WindsorContainer();
         var endpointConfiguration = new EndpointConfiguration("Samples.RavenDB.Server");
+        endpointConfiguration.UseContainer<WindsorBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingContainer(container);
+            });
         endpointConfiguration.SendFailedMessagesTo("error");
         using (var documentStore = new DocumentStore
         {
@@ -31,7 +55,8 @@ class Program
             persistence.SetDefaultDocumentStore(documentStore);
             persistence.EnableClusterWideTransactions();
 
-            endpointConfiguration.RegisterComponents(c => c.ConfigureComponent<IDocumentStore>(() => documentStore, DependencyLifecycle.SingleInstance));
+            var registration = Component.For<IDocumentStore>().Instance(documentStore);
+            container.Register(registration);
 
             #endregion
 
